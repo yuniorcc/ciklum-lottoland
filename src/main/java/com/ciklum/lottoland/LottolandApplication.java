c711809b091ce39225072e0b4434c6da0f0f1726
Yunior <yunior.camacho.castillo@gmail.com>
2021-09-17 14:59:26 +0200

    Initial project structure

---
+++ src/main/java/com/ciklum/lottoland/LottolandApplication.java
@@ -0,0 +1,13 @@
+package com.ciklum.lottoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottolandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciklum/lottoland/LottolandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciklum.lottoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
